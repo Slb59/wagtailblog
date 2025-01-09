9cf42d8eff28212ff52ed8f49a35d4c4574873a4
slb59 <osynia.devapps@gmail.com>
2025-01-09 22:30:31 +0100

    init wagtail blog

---
+++ wagtailblog/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-3+hfyr$np=j9_l%_1kdgpo%58v9r#uzn2gsc3*gpt&$qyfx49j"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
